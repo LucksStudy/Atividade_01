900f0e2027accf5baf804e68666b0baf7469f27a
LucksStudy <tad-lucassilva@ucpparana.edu.br>
2023-08-11 20:04:19 -0300

    v4

---
+++ Calculadora.py
@@ -0,0 +1,13 @@
+sair = "sair"
+
+numero01= input ("digite o primerio numero")
+numero02= input ("digite o segndo numero")
+operador = input("digite o operador ex:+,-,*")
+
+numero01_float = 0
+numero02_float = 0
+
+numero01_float = float(numero01)
+numero02_float = float (numero02)
+
+
