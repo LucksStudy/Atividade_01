ff1339bc08edd6a919ce8edf9e9d222be6efeccc
LucksStudy <tad-lucassilva@ucpparana.edu.br>
2023-08-11 21:25:31 -0300

    v1

---
+++ Valores.py
@@ -0,0 +1,10 @@
+print ("bem vindo ao calculador de Valores")
+
+valor1 = float(input("insira um valor"))
+valor2 = float(input("insira outro valor"))
+
+if valor1>valor2:
+    print ("O primeiro valor é mais alto que o segundo valor")
+else:
+    valor2>valor1
+    print ("O segundo valor é mais alto que o primeiro valor")
