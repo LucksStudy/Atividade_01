fb09564e13ced181043b395222d02f18938eb430
LucksStudy <tad-lucassilva@ucpparana.edu.br>
2023-08-11 21:14:32 -0300

    v4

---
+++ Calc_IMC.py
@@ -0,0 +1,21 @@
+print ("Bem vindo a calculadora de IMC (indice de Massa Corporal).")
+altura = float(input ("digite sua altura!"))
+peso = float(input("digite seu peso"))
+
+IMC = peso/(altura*altura)
+
+print (f"Seu IMC é" ,IMC)
+if IMC<=18.5:
+    print("você esta com magreza")
+elif IMC>=18.6 and IMC<=24.9:
+    print ("você esta normal")
+elif IMC>=25 and IMC<=29.9:
+    print ("Você esta com sobrepeso")
+elif IMC>=30 and IMC<=34.9:
+    print ("Você esta com obesidade I")
+elif IMC>=35 and IMC<=39.9:
+    print ("Você esta com obesidade II")
+
+else: 
+ IMC>=40
+ print ("Você esta com obesidade III, Procure um medico!")
--- Calculadora.py
+++ Calculadora.py
@@ -1,13 +1,14 @@
 sair = "sair"
 
-numero01= input ("digite o primerio numero")
-numero02= input ("digite o segndo numero")
-operador = input("digite o operador ex:+,-,*")
+numero01= input ("  digite o primerio numero")
+numero02= input ("  digite o segndo numero")
+operador = input ("  digite o operador")
 
 numero01_float = 0
 numero02_float = 0
 
 numero01_float = float(numero01)
-numero02_float = float (numero02)
+numero02_float = float(numero02)
 
-
+if operador == "+":
+    print(f"{numero01_float} + {numero02_float} =",numero02_float + numero01_float)
