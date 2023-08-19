343f92c2ca14ea33ac77d0e818eeec15a50aac0a
LucksStudy <tad-lucassilva@ucpparana.edu.br>
2023-08-18 22:08:31 -0300

    pipi

--- aula_18-08.py/ATV-07.py
+++ aula_18-08.py/ATV-07.py
@@ -6,3 +6,15 @@ print(lista1[4])
 lista2 = lista1*2
 
 print (lista2)
+
+lista1.append ("amanda") #adicionar elemento na lista exemplo um novo nome!
+print (lista1)
+
+lista1.remove("Caio") #remover elemento da lista
+print (lista1)
+
+lista1.pop(3) #Remover elemento da lista também
+print (lista1)
+
+for listageral in lista1:
+    print (listageral)
