2216847e0dbfc77d1afcba911ca2ccd546bbde82
LucksStudy <tad-lucassilva@ucpparana.edu.br>
2023-08-18 21:35:39 -0300

    At2

--- aula_18-08.py/ATV-07.py
+++ aula_18-08.py/ATV-07.py
@@ -1,4 +1,8 @@
 lista1 = ["Dieimes", "Nunes", "Souza", "Caio","Luana"]
 print (type(lista1))
-print(len(lista1))  #imprimir a lista
+print(len(lista1))  #imprimir qt itens lista
 print(lista1[4])
+
+lista2 = lista1*2
+
+print (lista2)
