1b4c9698c915bd5a5c3a484f401cfdb4b18481b9
EduardaCicatt0 <tad-eduardacicatto2@ucpparana.edu.br>
2023-08-26 22:20:06 -0300

    Giga_ATT1

--- Atividade_02/Atividade04.py
+++ Atividade_02/Atividade04.py
@@ -1,6 +1,34 @@
-#Desenvolva um script em Python que faça uma lista de compras, com o tipo de dados LIST.
-#O usuário deve ter a possibilidade: de inserir, apagar, editar e listar os conteúdos da sua lista.
+lista_compras = []
 
-lista=[]
+while True:
+    print("\nOpções:")
+    print("1. Adicionar item")
+    print("2. Listar itens")
+    print("3. Editar item")
+    print("4. Excluir item")
+    print("5. Sair")
 
-item=int(input(f"Digite uma das opções a seguir\n 1-Inserir \n 2-Apagar \n 3-Editar \n 4-Listar \n 5-Sair \n"))
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        item = input("Digite o item a ser adicionado: ")
+        lista_compras.append(item)
+        print(f"{item} foi adicionado à lista.")
+    elif opcao == "2":
+        for item in lista_compras:
+            print(item)
+    elif opcao == "3":
+        indice = int(input("Digite o índice do item a ser editado: "))
+        novo_item = input("Digite o novo item: ")
+        lista_compras[indice] = novo_item
+    elif opcao == "4":
+        indice = int(input("Digite o índice do item a ser excluído: "))
+        if indice < len(lista_compras):
+            item_removido = lista_compras.pop(indice)
+            print(f"{item_removido} foi removido da lista.")
+        else:
+            print("Índice inválido.")
+    elif opcao == "5":
+        break
+    else:
+        print("Opção inválida.")
